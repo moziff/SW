bd6de5c87deb0b4293df7c5f6bbd6e0e425473f2
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro-2.local>
2017-03-02 13:55:27 -0500

    added sounds

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	public void CharacterSelected(){
+		audio.Play ();
+	}
+}
