67011dbbd3ad1bc8ef0740ac642b255e280e24bc
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro-2.local>
2017-03-02 12:52:41 -0500

    fixed leftright arrows and it looks nice

---
+++ Assets/JsonParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class JsonParser : MonoBehaviour {
+
+	public int id;
+	public Text name;
+	public Text height;
+	public Text weight;
+
+	private string jsonData;
+
+	IEnumerator Start () {
+		
+		string url = "https://gist.githubusercontent.com/anonymous/4db1ffcbf9ab5e576dfa10e8b1b089b4/raw/846638148b2a9a8b48c60c7de13fe83507c087bf/star_store.json";
+		WWW www = new WWW (url);
+		yield return www;
+
+		if (www.error == null) {
+			jsonData = www.text;
+		}
+
+
+		JSONNode jsonNode = SimpleJSON.JSON.Parse(jsonData);
+
+//		print (jsonNode[id-1]["name"]);
+		name.text = jsonNode [id - 1] ["name"] + " - $" + jsonNode [id - 1] ["price"];
+		height.text = "Height - "+ jsonNode [id - 1] ["height"];
+		weight.text = "Weight - "+ jsonNode [id - 1] ["weight"];
+
+	}
+
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+	public GameObject[] waypoints;
+	public GameObject[] info;
+
+	private int index;
+
+	// Use this for initialization
+	void Start () {
+		index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			
+			if (index == 0) {
+				info [info.Length - 1].gameObject.SetActive (false);
+			} else {
+				info [index - 1].gameObject.SetActive (false);
+			}
+			index++;
+			if (index == waypoints.Length) {
+				index = 0;
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+				StartCoroutine(MoveToPosition(transform, waypoints[index].transform.position, 0.2f));	
+			} else { 
+				info [index - 1].gameObject.SetActive (true);
+				StartCoroutine(MoveToPosition(transform, waypoints[index].transform.position, 0.2f));
+			
+
+			}
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			
+			if (index == 0) {
+				info [1].gameObject.SetActive (false);
+			} else {
+				info [index - 1].gameObject.SetActive (false);
+			}
+			index--;
+
+			if (index < 0) {
+				index = index+7;
+			}
+
+			if (index == 0) {
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+				StartCoroutine(MoveToPosition(transform, waypoints[index].transform.position, 0.2f));	
+			} else { 
+				
+				info [index - 1].gameObject.SetActive (true);
+				StartCoroutine(MoveToPosition(transform, waypoints[index].transform.position, 0.2f));
+
+			}
+		}
+		
+	}
+
+	public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+	{
+		var currentPos = transform.position;
+		var t = 0f;
+		while(t < 1)
+		{
+			t += Time.deltaTime / timeToMove;
+			transform.position = Vector3.Lerp(currentPos, position, t);
+			yield return null;
+			if (index == 0) {
+				transform.rotation = Quaternion.Euler (0, 0, 0);
+			} else {
+				transform.LookAt(info[index-1].transform);
+			}
+		}
+
+	}
+
+}
